e5753767843095ff2d211ad2abdeced8e0396cb9
Unrecognized <tw.unrecognized@gmail.com>
2022-06-01 04:03:03 -0300

    reconnect

--- tamper.js
+++ tamper.js
@@ -3,6 +3,7 @@
 // @description  Troca de aldeia e navega entre as páginas farm, scavenge, main(principal) e chegando.
 // @author       Clezio Marcos
 // @match        https://*.tribalwars.com.br/game.php*
+// @include      https://tribalwars.com.br/*
 // @exclude      https://brs1.tribalwars.com.br/game.php*
 // @icon         https://www.vemtranquilo.host/VT/ÍCONES-GIFS/VEM TRANQUILO.jpg
 // @grant        none
@@ -11,7 +12,7 @@
 (async function() {
     'use strict'
     const url = [
-        'https://raw.githubusercontent.com/UnrecognizedBR/Test-Vscode/master/public/letsGO.js'
+        'https://raw.githubusercontent.com/UnrecognizedBR/Test-Vscode/master/public/letsGO.js',
     ]
     for (const key in url) {
         const data = await fetch( url[ key ] ).then(function(data) {
