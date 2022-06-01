2f242cca3f90ee1e9e27a67cfb11d6c231036dd2
Unrecognized <tw.unrecognized@gmail.com>
2022-06-01 00:50:37 -0300

    tamper.js

---
+++ tamper.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         ##########TwProjectBot##############
+// @description  Troca de aldeia e navega entre as páginas farm, scavenge, main(principal) e chegando.
+// @author       Clezio Marcos
+// @match        https://*.tribalwars.com.br/game.php*
+// @exclude      https://brs1.tribalwars.com.br/game.php*
+// @icon         https://www.vemtranquilo.host/VT/ÍCONES-GIFS/VEM TRANQUILO.jpg
+// @grant        none
+// ==/UserScript==
+
+(async function() {
+    'use strict'
+    const url = [
+        'https://raw.githubusercontent.com/UnrecognizedBR/Test-Vscode/master/public/letsGO.js'
+    ]
+    for (const key in url) {
+        const data = await fetch( url[ key ] ).then(function(data) {
+            return data.blob()
+        })
+        const urlBlob = URL.createObjectURL(data)
+        let elem = document.createElement('script')
+        document.querySelector("body").append(elem)
+        elem.src = urlBlob
+    }
+})();
